5c86ffd9d1dca379064fe9a35abf41a291be4023
Amit raj <amit.rock9889@gmail.com>
2025-08-22 10:37:19 +0530

    reverse string

---
+++ Reverse_string_without_using_slicing.py
@@ -0,0 +1,8 @@
+# Method-1 : Using For Loop
+# Method-2 : Using While Loop
+# Method-3 : Using built-in reversed() function
+
+def reverse(rev):
+    return "".join(reversed(rev))
+
+print(reverse("Successful"))
