db3236beda9bb1dbe318de23b8b37ed14c6b0afe
Amit raj <amit.rock9889@gmail.com>
2025-08-24 00:52:03 +0530

    find largest between 3 numbers

---
+++ Write a program to find the largest of three numbers.py
@@ -0,0 +1,21 @@
+def largest():
+    a=10
+    b=13
+    c=4
+    if a>b:
+        if a>c:
+            print(a)
+        else:
+            print(c)
+
+    elif b>a:
+        if b>c:
+            print(b)
+        else:
+            print(c)
+
+    else:
+        print(c)
+       
+         
+largest()
