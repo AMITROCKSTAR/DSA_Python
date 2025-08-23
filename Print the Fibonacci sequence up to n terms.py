83a38b3fc760d9b79bf666438fadc855bb9a0c87
Amit raj <amit.rock9889@gmail.com>
2025-08-24 01:26:22 +0530

    Fibonacci sequence

---
+++ Print the Fibonacci sequence up to n terms.py
@@ -0,0 +1,16 @@
+def fibonacci(num):
+    # num=5
+    a=0
+    b=1
+    c=0
+   
+    for i in range(num):
+        c=a+b
+        print(a," ")
+        a=b
+        b=c
+    
+    
+
+number = int(input())
+fibonacci(number)
