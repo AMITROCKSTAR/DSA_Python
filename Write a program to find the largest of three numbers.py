40356dd7a0ad4584cfe96783640d7aab71305b6c
Amit raj <amit.rock9889@gmail.com>
2025-08-24 00:55:57 +0530

    Solved with built-in python max function

--- Write a program to find the largest of three numbers.py
+++ Write a program to find the largest of three numbers.py
@@ -2,6 +2,12 @@ def largest():
     a=10
     b=13
     c=4
+
+    # Python built-in max function
+
+    print(max(a,b,c))
+
+    ## Writing logic to print largest between 3 numbers
     if a>b:
         if a>c:
             print(a)
